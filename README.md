9f2f20e2e039823cdb42f7f9fa29980ab81e5b79
Kent C. Dodds <kent@doddsfamily.us>
2017-02-01 10:15:39 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# jsx-indent-bug
+
+This demonstrates a bug with `jsx-indent`.
+
+1. Install dependencies
+2. Run the `fix` script
+3. Run the `lint` script
+
+See the output is not totally fixed and linting still passes
---
+++ index.js
@@ -0,0 +1,14 @@
+const React = require('react')
+module.exports = MyComponent
+
+function MyComponent(props) {
+  return (
+    <div
+      className="foo-bar"
+      id="thing"
+    >
+      Hello world!
+    </div>
+  )
+}
+
